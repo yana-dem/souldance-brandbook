2bbc368bc8a41fcac03d5858dc626aacc7d9dfce
Яна Демьяновская <ianademianovskaia@MacBook-Air-Ana.local>
2026-06-16 16:59:49 +0300

    Remove system files, add .gitignore

--- serve.py
+++
@@ -1,5 +0,0 @@
-import os
-import sys
-os.chdir('/Users/ianademianovskaia/souldance')
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-HTTPServer(('', 3000), SimpleHTTPRequestHandler).serve_forever()
